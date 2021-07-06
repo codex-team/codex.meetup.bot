5486fc936a3b027953b18e4ea938172fc424fc73
Nikita Melnikov <nikmel2803@gmail.com>
2021-07-06 09:15:56 +0300

    remove files with test

--- tests/create_server.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-
-from src.services.servers_controller import servers_controller
-
-loop = asyncio.get_event_loop()
-
-gather = asyncio.gather(servers_controller.create_server(123123123, f'server{i}') for i in range(5))
-
-loop.run_until_complete(gather)
