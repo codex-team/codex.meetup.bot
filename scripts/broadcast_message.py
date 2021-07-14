ee1eaaf4e3c3234e981a3c40831b34a39b5b53f1
Nikita Melnikov <nikmel2803@gmail.com>
2021-07-14 16:21:23 +0300

    add BROADCAST_TEST_MODE var

--- scripts/broadcast_message.py
+++ scripts/broadcast_message.py
@@ -1,5 +1,6 @@
 from src.bot import bot
 from src.services.database import database
+from src.services.env import BROADCAST_TEST_MODE
 
 message = """
 Напоминаем, что вы зарегистрированы на Codex Meetup: Ansible, который пройдёт завтра, в 19:00 в корпусе ИТМО на Песочной набережной 14, ауд. 308. 
@@ -10,6 +11,6 @@ message = """
 """
 
 if __name__ == '__main__':
-    query = {"is_registered": True}
+    query = {"is_admin": True} if BROADCAST_TEST_MODE else {"is_registered": True}
     for user in database.users.find(query):
         bot.sendMessage(user.get("id"), text=message)
