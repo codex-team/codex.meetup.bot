b5e1dc92b04d4929f1db1418495b6c0730f9a533
Nikita Melnikov <nikmel2803@gmail.com>
2021-07-14 16:23:16 +0300

    fix typo

--- scripts/broadcast_message.py
+++ scripts/broadcast_message.py
@@ -3,7 +3,7 @@ from src.services.database import database
 from src.services.env import BROADCAST_TEST_MODE
 
 message = """
-Напоминаем, что вы зарегистрированы на Codex Meetup: Ansible, который пройдёт завтра, в 19:00 в корпусе ИТМО на Песочной набережной 14, ауд. 308. 
+Напоминаем, что вы зарегистрированы на CodeX Meetup: Ansible, который пройдёт завтра, в 19:00 в корпусе ИТМО на Песочной набережной 14, ауд. 308. 
 
 Для участия вам потребуется ноутбук с установленным Ansible.
 
